f19f0e087b0460a9cabfee6a4ff2e7e61c563ca2
Alexander Maru <marua7665@gmail.com>
2015-06-22 23:49:50 -0700

    sticky nav bar success

--- custom.js
+++ custom.js
@@ -1,7 +1,11 @@
-$(function() {
-    $('#wrapper').height($("#main-nav").height());
-    
-    $('#main-nav').affix({
-        offset: { top: $('#nav').offset().top }
-    });
+var  mn = $(".main-nav");
+    mns = "main-nav-scrolled";
+    hdr = $('header').height();
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() > hdr ) {
+    mn.addClass(mns);
+  } else {
+    mn.removeClass(mns);
+  }
 });
