f753e36734cb240964894408c67aa82c8f4f97b8
Alexander Maru <marua7665@gmail.com>
2015-06-24 14:17:37 -0700

    page update

--- Trial/trial.js
+++ Trial/trial.js
@@ -9,3 +9,34 @@ $(window).scroll(function() {
     mn.removeClass(mns);
   }
 });
+
+// using on
+$('#my_elem').on('mousewheel', function(event) {
+    console.log(event.deltaX, event.deltaY, event.deltaFactor);
+});
+
+// using the event helper
+$('#my_elem').mousewheel(function(event) {
+    console.log(event.deltaX, event.deltaY, event.deltaFactor);
+});
+
+var express = require('express');
+var app = express.createServer();
+
+app.use(require('browserify')({
+    require : [ 'jquery-browserify', 'jquery-mousewheel' ]
+}));
+
+var $ = require('jquery-browserify');
+require('jquery-mousewheel')($);
+
+$(function () {
+  $.srSmoothscroll({
+    // defaults
+    step: 55,
+    speed: 400,
+    ease: 'swing',
+    target: $('body'),
+    container: $(window)
+  })
+})
