d2f895aa6cae2ce8b045a1b6d7e68d4fe1782b2f
Alexander Maru <marua7665@gmail.com>
2015-06-25 15:45:05 -0700

    first attempt at normalizing scroll

--- Stylesheets/main.js
+++ Stylesheets/main.js
@@ -10,13 +10,10 @@ $(window).scroll(function() {
   }
 });
 
-/*
-if (window.addEventListener) window.addEventListener('DOMMouseScroll', wheel, false);
-window.onmousewheel = document.onmousewheel = wheel;
 
 function wheel(event) {
     var delta = 0;
-    if (event.wheelDelta) delta = event.wheelDelta / 120;
+    if (event.wheelDelta) delta = event.wheelDelta / 500;
     else if (event.detail) delta = -event.detail / 3;
 
     handle(delta);
@@ -25,10 +22,25 @@ function wheel(event) {
 }
 
 function handle(delta) {
-    var time = 1000;
-	var distance = 400;
+    var time = 500;
+	var distance = 800;
     
     $('html, body').stop().animate({
         scrollTop: $(window).scrollTop() - (distance * delta)
     }, time );
-}*/
+}
+
+function normalizeWheelSpeed(event) {
+    var normalized;
+    if (event.wheelDelta) {
+        normalized = (event.wheelDelta % 120 - 0) == -0 ? event.wheelDelta / 120 : event.wheelDelta / 12;
+    } else {
+        var rawAmmount = event.deltaY ? event.deltaY : event.detail;
+        normalized = -(rawAmmount % 3 ? rawAmmount * 10 : rawAmmount / 3);
+    }
+    console.log(normalized);
+    return normalized;
+}
+window.addEventListener('mousewheel', normalizeWheelSpeed);
+window.addEventListener('DOMMouseScroll', normalizeWheelSpeed);
+window.addEventListener('wheel', normalizeWheelSpeed);
