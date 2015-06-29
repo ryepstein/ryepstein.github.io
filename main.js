19a7cb54c2f989d744f24a16d26e33b71f0ebc55
Alexander Maru <marua7665@gmail.com>
2015-06-28 20:06:57 -0700

    sliding reconfiguration

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ function showMenu(){
  function hideMenu(){
    document.getElementById("menu").style.display="none";
  }
+
+ var  mn = $(".main-nav");
+    mns = "main-nav-scrolled";
+    hdr = $('header').height();
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() > hdr ) {
+    mn.addClass(mns);
+  } else {
+    mn.removeClass(mns);
+  }
+});
