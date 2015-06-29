6f8b594a546386444072749d6c56968528a71766
Alexander Maru <marua7665@gmail.com>
2015-06-28 17:53:48 -0700

    navbar configuration

--- main.js
+++
@@ -1,7 +0,0 @@
-$('#navbar-btn').click(function(){
-        $('nav ul').slideToggle('slow', function(){
-            $(this).css('display') == 'none' ? 
-            $(this).removeClass('showNav').addClass('hideNav').removeAttr('style') : 
-            $(this).removeClass('hideNav').addClass('showNav').removeAttr('style');
-    });
-})
