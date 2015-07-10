a3b49220912623f5f2d9ffb2bf8feea30f59ad48
Alexander Maru <marua7665@gmail.com>
2015-07-10 16:48:41 +0000

    added helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,37 +4,37 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home"
+    assert_select "title", "Kidanemihret Ethiopian Orthodox Tewahedo Church"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About"
+    assert_select "title", "About | Kidanemihret Ethiopian Orthodox Tewahedo Church"
   end
 
   test "should get teachings" do
     get :teachings
     assert_response :success
-    assert_select "title", "Teachings"
+    assert_select "title", "Teachings | Kidanemihret Ethiopian Orthodox Tewahedo Church"
   end
 
   test "should get media" do
     get :media
     assert_response :success
-    assert_select "title", "Media"
+    assert_select "title", "Media | Kidanemihret Ethiopian Orthodox Tewahedo Church"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact"
+    assert_select "title", "Contact | Kidanemihret Ethiopian Orthodox Tewahedo Church"
   end
 
   test "should get youth" do
     get :youth
     assert_response :success
-    assert_select "title", "Youth"
+    assert_select "title", "Youth | Kidanemihret Ethiopian Orthodox Tewahedo Church"
   end
 
 end
