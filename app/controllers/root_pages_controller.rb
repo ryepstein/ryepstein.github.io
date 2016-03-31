3fd5a0b506f785397b58bd25f22b533de03f2c15
Alexander Maru <marua7665@gmail.com>
2016-03-30 21:27:21 -0400

    pdf render fix #2

--- app/controllers/root_pages_controller.rb
+++ app/controllers/root_pages_controller.rb
@@ -7,9 +7,4 @@ class RootPagesController < ApplicationController
 
   def resume
   end
-
-  def pdf
-    pdf_filename = File.join(Rails.root, "tmp/resume.pdf")
-    send_file(pdf_filename, :filename => "resume.pdf", :disposition => 'inline', :type => "application/pdf")
-  end
 end
