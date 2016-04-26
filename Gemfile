ce1903ca425c10fba30c770e0769811cef1f658e
Alexander Maru <marua7665@gmail.com>
2016-04-26 15:04:57 -0400

    contacts init

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'google-webfonts-rails',   '~> 0.0.4'
 gem 'sprockets-rails', :require => 'sprockets/railtie'
 gem 'bootstrap', '~> 4.0.0.alpha3'
 gem 'font-awesome-rails'
+gem 'mail_form'
+gem 'simple_form'
 
 group :development, :test do
   # Use sqlite3 as the database for Active Record
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   get 'resume'   => 'root_pages#resume'
 
+  match '/contacts',     to: 'contacts#new',             via: 'get'
+  resources "contacts", only: [:new, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
