47b10a27b8f1e70f52d98fbbc7944532d3df33fa
Alexander Maru <marua7665@gmail.com>
2016-04-26 20:40:53 -0400

    updated contact+links

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   get 'resume'   => 'root_pages#resume'
 
+  match '/contacts',     to: 'root_pages#create',             via: 'post'
+  resources "contacts", only: [:home, :create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
