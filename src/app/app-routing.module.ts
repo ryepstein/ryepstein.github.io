b1cba69e0447226271d0d89b1ff0bf43cedc7d65
assefamaru <me@alexandermaru.com>
2017-10-05 17:48:31 -0400

    Add home and 404 components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent }   from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './404/not-found.component';
 
 const routes: Routes = [
-    { path: '',  component: AppComponent },
+    { path: '',  component: HomeComponent },
+    { path: '**',  component: NotFoundComponent },
 ];
 
 @NgModule({
